47f2fda428ef3dee5e263c453b07c06218c88540
EunJung803 <aimee3322@gmail.com>
2022-07-05 17:24:31 +0900

    사칙연산 구현

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -7,29 +7,58 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void 더하기() {
-        int rs = Calculator.run("10 + 20");
+        int rs = Calculator.run("1 + 1");
 
-        assertEquals(30, rs);
+        assertEquals(2, rs);
     }
 
     @Test
     public void 더하기2() {
-        int rs = Calculator.run("20 + 20");
+        int rs = Calculator.run("1 + 2");
 
-        assertEquals(40, rs);
+        assertEquals(3, rs);
+    }
+
+
+    @Test
+    public void 빼기() {
+        int rs = Calculator.run("1 - 2");
+
+        assertEquals(-1, rs);
+    }
+
+    @Test
+    public void 곱하기() {
+        int rs = Calculator.run("1 * 2");
+
+        assertEquals(2, rs);
+    }
+
+    @Test
+    public void 나누기() {
+        int rs = Calculator.run("4 / 2");
+
+        assertEquals(2, rs);
     }
 
     @Test
     public void 더하기3() {
-        int rs = Calculator.run("10 + 10");
+        int rs = Calculator.run("1 + 2 + 3");
 
-        assertEquals(20, rs);
+        assertEquals(6, rs);
     }
 
     @Test
-    public void 빼기() {
-        int rs = Calculator.run("10 - 10");
+    public void 더하기_곱하기() {
+        int rs = Calculator.run("1 + 2 * 3");
 
-        assertEquals(0, rs);
+        assertEquals(7, rs);
+    }
+
+    @Test
+    public void 괄호계산() {
+        int rs = Calculator.run("(1 + 2) * 3");
+
+        assertEquals(9, rs);
     }
 }
--- src/test/java/com/ll/exam/Calculator.java
+++ src/test/java/com/ll/exam/Calculator.java
@@ -3,16 +3,39 @@ package com.ll.exam;
 public class Calculator {
     public static int run(String s) {
         boolean isMinus = s.indexOf(" - ") != -1;
+        boolean isMultiply = s.indexOf(" * ") != -1;
+        boolean isDivision = s.indexOf(" / ") != -1;
 
-        if (isMinus) {
+        if (isMinus) { // -가 있으면 - 진행
             return runMinus(s);
+        }
+        if(isMultiply) {
+            return runMultiply(s);
+        }
+        if(isDivision) {
+            return runDivision(s);
+        }
+
+        if(s.length() > 5) {
+            return runTriplePlus(s);
         }
 
         return runPlus(s);
     }
 
+
+    private static int runTriplePlus(String s) {
+        String[] sBits = s.split(" \\+ "); // + 로 문자열 split
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+        int no3 = Integer.parseInt(sBits[2]);
+
+
+        return no1 + no2 + no3;
+    }
+
     private static int runPlus(String s) {
-        String[] sBits = s.split(" \\+ ");
+        String[] sBits = s.split(" \\+ "); // + 로 문자열 split
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
 
@@ -20,10 +43,26 @@ public class Calculator {
     }
 
     private static int runMinus(String s) {
-        String[] sBits = s.split(" \\- ");
+        String[] sBits = s.split(" \\- "); // - 로 문자열 split
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
 
         return no1 - no2;
+    }
+
+    private static int runMultiply(String s) {
+        String[] sBits = s.split(" \\* "); // * 로 문자열 split
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 * no2;
+    }
+
+    private static int runDivision(String s) {
+        String[] sBits = s.split(" \\/ "); // * 로 문자열 split
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 / no2;
     }
 }
